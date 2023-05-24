1248a1cc3950b87dc05eb222dc01b30163c317f2
danielefongo <danielefongo@gmail.com>
2023-05-24 20:47:14 +0200

    chore: create dummy buf on preview

--- lua/microscope-buffers/parsers.lua
+++ lua/microscope-buffers/parsers.lua
@@ -5,6 +5,7 @@ function parsers.buffer(data)
   local elements = vim.split(data.text, ": ", {})
 
   data.buffer = tonumber(elements[1]) + 0
+  data.file = elements[2]
   data.highlights =
     highlight.new(data.highlights, data.text):hl_match(highlight.color.color1, "(%d+:)(.*)", 1):get_highlights()
 
--- lua/microscope-buffers/preview.lua
+++ lua/microscope-buffers/preview.lua
@@ -1,3 +1,10 @@
+local highlight = require("microscope.utils.highlight")
+
 return function(data, window)
-  window:set_buf(data.buffer)
+  local lines = vim.api.nvim_buf_get_lines(data.buffer, 0, -1, false)
+  local row = vim.fn.getbufinfo(data.buffer)[1].lnum
+
+  window:write(lines)
+  highlight(data.file, window.buf)
+  window:set_cursor({ row, 0 })
 end
