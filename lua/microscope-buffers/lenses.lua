9291d8e24ac3bd0d918822ebf8ec982a4a6e1ce7
danielefongo <danielefongo@gmail.com>
2024-04-24 18:00:39 +0200

    feat: use new flow consuming api

--- lua/microscope-buffers/lenses.lua
+++ lua/microscope-buffers/lenses.lua
@@ -3,20 +3,18 @@ local lenses = {}
 function lenses.listed()
   return {
     fun = function(flow)
-      flow.cmd
-        .fn(function()
-          local bufs = {}
-          local buf_ids = vim.api.nvim_list_bufs()
+      flow.consume(flow.cmd.fn(function()
+        local bufs = {}
+        local buf_ids = vim.api.nvim_list_bufs()
 
-          for _, id in ipairs(buf_ids) do
-            if vim.fn.buflisted(id) == 1 then
-              table.insert(bufs, id .. ": " .. vim.api.nvim_buf_get_name(id))
-            end
+        for _, id in ipairs(buf_ids) do
+          if vim.fn.buflisted(id) == 1 then
+            table.insert(bufs, id .. ": " .. vim.api.nvim_buf_get_name(id))
           end
+        end
 
-          return bufs
-        end)
-        :into(flow)
+        return bufs
+      end))
     end,
   }
 end
