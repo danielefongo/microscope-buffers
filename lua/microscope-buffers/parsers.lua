3682e8933c08ec4b92b800c14b4669024354898f
danielefongo <danielefongo@gmail.com>
2024-12-23 08:14:02 +0100

    feat: show relative path instead of full path

--- lua/microscope-buffers/parsers.lua
+++ lua/microscope-buffers/parsers.lua
@@ -1,15 +1,25 @@
 local highlight = require("microscope.api.highlight")
 local parsers = {}
 
+local function relative_path(path)
+  local current_path = vim.fn.getcwd():gsub("[%-%.%+%[%]%(%)%$%^%%%?%*]", "%%%1") .. "/"
+  return string.gsub(path, current_path, "")
+end
+
 function parsers.buffer(data)
   local elements = vim.split(data.text, ": ", {})
+  local limited_path = relative_path(data.text)
+  print(vim.inspect({ full = data.text, relative = limited_path }))
 
-  data.buffer = tonumber(elements[1]) + 0
-  data.file = elements[2]
-  data.highlights =
-    highlight.new(data.highlights, data.text):hl_match(highlight.color.color1, "(%d+:)(.*)", 1):get_highlights()
-
-  return data
+  return {
+    text = limited_path,
+    highlights = highlight
+      .new(data.highlights, limited_path)
+      :hl_match(highlight.color.color1, "(%d+:)(.*)", 1)
+      :get_highlights(),
+    buffer = tonumber(elements[1]) + 0,
+    file = elements[2],
+  }
 end
 
 return parsers
