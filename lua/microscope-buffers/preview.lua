aa2aeb502574b2a7ecc996f1327f8017358d09da
danielefongo <danielefongo@gmail.com>
2025-03-20 10:34:13 +0100

    feat: use the new treesitter api

--- lua/microscope-buffers/preview.lua
+++ lua/microscope-buffers/preview.lua
@@ -1,10 +1,12 @@
-local highlight = require("microscope.utils.highlight")
+local treesitter = require("microscope.api.treesitter")
 
 return function(data, window)
   local lines = vim.api.nvim_buf_get_lines(data.buffer, 0, -1, false)
   local row = vim.fn.getbufinfo(data.buffer)[1].lnum
 
   window:write(lines)
-  highlight(data.file, window.buf)
+  local hls = treesitter.for_buffer(window.buf, vim.filetype.match({ filename = data.file, buf = window.buf }))
+  window:clear_buf_hls()
+  window:set_buf_hls(hls)
   window:set_cursor({ row, 0 })
 end
